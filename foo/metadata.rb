b764da2e4ad12e8af268a3012a074a3f7dff6cc6
Daniel Spangenberg <danielsp@amazon.com>
2013-07-30 18:55:30 +0200

    add metadata

---
+++ foo/metadata.rb
@@ -0,0 +1,9 @@
+name             "getting-started"
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Used by the Fast Start guides at wiki.opscode.com"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.4.0"
+
+depends 'bang'
